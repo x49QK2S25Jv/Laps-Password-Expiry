b2d459ff0f638a5f9f95cd3153c51d96001dddf7
x49QK2S25Jv <46139035+x49QK2S25Jv@users.noreply.github.com>
2019-02-07 09:37:05 +1300

    Update LAPS-Password-Expiry.ps1

--- LAPS-Password-Expiry.ps1
+++ LAPS-Password-Expiry.ps1
@@ -1,1 +1,4 @@
-﻿Get-AdmPwdPassword -ComputerName * | Select-Object ComputerName,ExpirationTimestamp
+﻿$Date = get-date -Format {dd/MM/yyyy hh:mm:ss}
+Get-AdmPwdPassword -ComputerName * | Select-Object ComputerName,ExpirationTimestamp | where ($_.ExpirationTimestamp -le $Date)
+
+
